ddd9ccd2455a9b8f9ec64bc9248244c49ed1f51d
Michał Żelazko <242149@edu.p.lodz.pl>
2023-03-05 21:08:05 +0100

    hello world web app

---
+++ IWA-MZelazko/src/main/java/pl/dmcs/iwamzelazko/IwaMZelazkoApplication.java
@@ -0,0 +1,13 @@
+package pl.dmcs.iwamzelazko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaMZelazkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IwaMZelazkoApplication.class, args);
+    }
+
+}
---
+++ IWA-MZelazko/src/test/java/pl/dmcs/iwamzelazko/IwaMZelazkoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dmcs.iwamzelazko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaMZelazkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
