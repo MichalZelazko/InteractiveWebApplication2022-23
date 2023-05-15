ce1fcc755a495d0a75ff5790d94c105db7d57ab6
MichalZelazko <242149@edu.p.lodz.pl>
2023-05-15 10:02:43 +0200

    Frontend security, work in progress

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -8,10 +8,31 @@ import { UserListComponent } from './user-list/user-list.component';
 import { ArticleComponent } from './article/article.component';
 import { QuadraticEquationComponent } from './quadratic-equation/quadratic-equation.component';
 import { HttpClientModule } from "@angular/common/http";
-import {appRoutingModule} from "./app.routing";
 import { RedditComponent } from './reddit/reddit.component';
 import { StudentsComponent } from './students/students.component';
 import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import {httpInterceptorProviders} from "./auth/auth-interceptor";
+import {RoleGuard} from "./guards/role.guard";
+import {authGuard} from "./guards/auth.guard";
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'quadratic-equation', component: QuadraticEquationComponent },
+  { path: 'reddit', component: RedditComponent },
+  { path: 'user', component: UserComponent, canActivate: [RoleGuard], data: { roles: ['ROLE_USER','ROLE_ADMIN'] },},
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard], data: { roles: ['ROLE_ADMIN'] },},
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
@@ -23,14 +44,19 @@ import { HomeComponent } from './home/home.component';
     QuadraticEquationComponent,
     RedditComponent,
     StudentsComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    appRoutingModule
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- IWA-MZelazko/src/main/java/pl/dmcs/iwamzelazko/security/services/UserDetailsServiceImpl.java
+++ IWA-MZelazko/src/main/java/pl/dmcs/iwamzelazko/security/services/UserDetailsServiceImpl.java
@@ -11,14 +11,18 @@ import pl.dmcs.iwamzelazko.repository.UserRepository;
 
 @Service
 public class UserDetailsServiceImpl  implements UserDetailsService {
+
     @Autowired
     UserRepository userRepository;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("User Not Found with -> username: " + username));
         return UserPrinciple.build(user);
     }
+
 }
+
